3416f27a2799c3133181050a9e215b0046baefed
ylimas <yagovianaalencar23@gmail.com>
2026-05-19 17:40:35 -0300

    Criar exer1

---
+++ cursoC/exer1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+
+//declaração de variáveis
+	int num;
+
+
+//ler o numero
+	printf("Digite um número inteiro: \n");
+	scanf("%d", &num);
+
+  //mostrar na tela
+	printf("\n\nO número digitado foi: %d", num);
+
+
+	return 0;
+
+}
