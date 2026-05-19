aaca38cfdcf8e08e8dd4e67420a5f5b0544cee79
ylimas <yagovianaalencar23@gmail.com>
2026-05-19 17:41:11 -0300

    criar exer2

---
+++ cursoC/exer2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+
+// declaração de variaveis
+	int num1;
+	int num2;
+	int num3;
+	int soma;
+
+// Entrada de  dados
+
+	printf("\nDigite o primeiro número: ");
+	scanf("%d", &num1);
+	
+	printf("\nDigite o segundo número: ");
+	scanf("%d", &num2);
+	
+	printf("\nDigite o terceiro número: ");
+	scanf("%d", &num3);
+
+	printf("\n\nCalculando o resultado...\n\n");
+
+	soma = num1 + num2 + num3;
+
+	printf("O seu resultado é: %d", &soma);
+
+	return 0;
+}
