436d79f522245c3f3f1397bb27f97ddfc1f4b430
ylimas <yagovianaalencar23@gmail.com>
2026-05-21 17:50:13 -0300

    Create exer6.c

---
+++ cursoC/exer6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Três amigos jogaram na loteria. Caso eles ganhem, o prêmio deve ser repartido proporcionalmente ao valor que cada um deu para a realização da aposta. Faça um programa que leia quanto cada apostador apostou, o valor do prêmio e imprima quanto cada um ganharia do prêmio com base no valor investido.
+
+int int main() {
+
+
+	float aposta1, aposta2, aposta3, premio, porcentagem1, porcentagem2, porcentagem3
+
+
+0
+
+
+	
+	return 0;
+}
