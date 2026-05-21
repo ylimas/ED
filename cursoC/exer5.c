b684bddb88a8d424e5b83b8f98983de2581793bd
ylimas <yagovianaalencar23@gmail.com>
2026-05-21 17:01:50 -0300

    final exer5

---
+++ cursoC/exer5.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+//Leia quatro notas, calcule a média aritmética e imprima o resultado. 
+
+
+
+
+
+int main() {
+
+// Declaração de Variáveis
+	float nota1;
+	float nota2;
+	float nota3;
+	float nota4;
+	
+	float media;
+
+// Entrada de Dados
+	
+	printf("\nInsira a primeira nota: ");
+	scanf("%f", &nota1);
+
+	printf("\nInsira a segunda nota: ");
+	scanf("%f", &nota2);
+
+	printf("\nInsira a terceira nota: ");
+	scanf("%f", &nota3);
+
+	printf("\nInsira a quarta nota: ");
+	scanf("%f", &nota4);
+
+// Processamento de dados
+
+	media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// Saída de Dados
+
+	printf("\nO resultado é: %.2f", media);
+
+
+	return 0;
+}#include <stdio.h>
+
+//Leia quatro notas, calcule a média aritmética e imprima o resultado. 
+
+
+
+
+
+int main() {
+
+// Declaração de Variáveis
+	float nota1;
+	float nota2;
+	float nota3;
+	float nota4;
+	
+	float media;
+
+// Entrada de Dados
+	
+	printf("\nInsira a primeira nota: ");
+	scanf("%f", &nota1);
+
+	printf("\nInsira a segunda nota: ");
+	scanf("%f", &nota2);
+
+	printf("\nInsira a terceira nota: ");
+	scanf("%f", &nota3);
+
+	printf("\nInsira a quarta nota: ");
+	scanf("%f", &nota4);
+
+// Processamento de dados
+
+	media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// Saída de Dados
+
+	printf("\nO resultado é: %.2f", media);
+
+
+	return 0;
+}
