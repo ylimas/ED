35fe6a5044ebf2d2bfb01fd51a5f6f7264cded9e
ylimas <yagovianaalencar23@gmail.com>
2026-05-22 06:45:38 -0300

    Update exer6.c
    Fix syntax errors and improve variable declarations.

--- cursoC/exer6.c
+++ cursoC/exer6.c
@@ -4,13 +4,29 @@
 
 int int main() {
 
-
+// Declaração de Variáveis
+	
 	float aposta1, aposta2, aposta3, premio, porcentagem1, porcentagem2, porcentagem3
 
+//Entrada
 
-0
+//Aposta
+	printf("\nEntre com o valor da primeira aposta: ");
+	scanf("%f", aposta1);
 
+	printf("\nEntre com o valor da segunda aposta: ");
+	scanf("%f", aposta2);
 
+	printf("\nEntre com o valor da terceira aposta: ");
+	scanf("%f", aposta3);
+
+//Premio
+
+	printf("\n\nEntre com o valor do prêmio: ");
+	scanf("%f", premio);
+//Processamento de dados
+
+	
 	
 	return 0;
 }
